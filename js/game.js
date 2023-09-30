8c85017cb18fd7b3ba4e0d0ff20f958b67823af4
Soh Hong Yu <sohhongyu@gmail.com>
2023-09-30 10:37:38 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -14,6 +14,7 @@ var elDiceOne = document.getElementById("dice1");
 var elDiceTwo = document.getElementById("dice2");
 
 function rollDice() {
+  document.getElementById("roll").innerHTML = 0;
   var diceOne = Math.floor(Math.random() * 6 + 1);
   var diceTwo = Math.floor(Math.random() * 6 + 1);
 
@@ -33,22 +34,35 @@ function rollDice() {
   setTimeout(() => {
     document.getElementById("roll").innerHTML = diceOne + diceTwo;
     currentPos += diceOne + diceTwo;
-    currentPos %= 36;
-    movePlayer(currentPos);
-    $("#diceModal").modal("hide");
-    for (var i = 1; i <= 6; i++) {
-      elDiceOne.classList.remove("show-" + i);
-      if (diceOne === 1) {
-        elDiceOne.classList.add("show-" + i);
-      }
+    if (currentPos >= 36) {
+      sessionStorage.setItem(
+        "point",
+        parseInt(sessionStorage.getItem("point")) + 100
+      );
+      setTimeout(() => {
+        alert("Earned 100 points!");
+      }, 700);
     }
+    setTimeout(() => {
+      $("#diceModal").modal("hide");
+      currentPos %= 36;
+      setTimeout(() => {
+        movePlayer(currentPos);
+      }, 500);
+      for (var i = 1; i <= 6; i++) {
+        elDiceOne.classList.remove("show-" + i);
+        if (diceOne === 1) {
+          elDiceOne.classList.add("show-" + i);
+        }
+      }
 
-    for (var k = 1; k <= 6; k++) {
-      elDiceTwo.classList.remove("show-" + k);
-      if (diceTwo === 1) {
-        elDiceTwo.classList.add("show-" + k);
+      for (var k = 1; k <= 6; k++) {
+        elDiceTwo.classList.remove("show-" + k);
+        if (diceTwo === 1) {
+          elDiceTwo.classList.add("show-" + k);
+        }
       }
-    }
+    }, 700);
   }, 1000);
 }
 
