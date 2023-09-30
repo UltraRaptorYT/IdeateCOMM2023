59d5c3169ca7eb40451c5bd339b73ef358f2626b
Soh Hong Yu <sohhongyu@gmail.com>
2023-09-30 10:46:25 +0800

    Faked Translate

--- js/translate.js
+++ js/translate.js
@@ -23,4 +23,39 @@ switchBtn.addEventListener("click", () => {
   }
 });
 
+document.getElementById("translateFrom").addEventListener("change", (e) => {
+  translate();
+});
 
+document.getElementById("translateTo").addEventListener("change", (e) => {
+  translate();
+});
+
+document.getElementById("translateMsg").addEventListener("input", (e) => {
+  translate();
+});
+
+function translate() {
+  if (
+    document.getElementById("translateFrom").value == "English" &&
+    document.getElementById("translateTo").value == "Hokkien"
+  ) {
+    if (
+      document.getElementById("translateMsg").value.toLowerCase() ==
+      "i dont know"
+    ) {
+      document.getElementById("outputMsg").innerHTML = `我不知。
+Góa m̄ chai.`;
+    } else if (
+      document.getElementById("translateMsg").value.toLowerCase() ==
+      "what your name"
+    ) {
+      document.getElementById("outputMsg").innerHTML = `你叫啥乜名。
+Lí kió siáⁿ-mí miâ?`;
+    } else {
+      document.getElementById("outputMsg").innerHTML = "Translating...";
+    }
+  } else {
+    document.getElementById("outputMsg").innerHTML = "Translating...";
+  }
+}
